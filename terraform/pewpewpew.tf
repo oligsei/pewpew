b80931edac80054d36a573b445fbd5b5515734b5
Alexey Reznikov <oligsei.reznikov@gmail.com>
2020-01-05 05:13:27 +0200

    initial

---
+++ terraform/pewpewpew.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  version = "~> 2.0"
+  region = "eu-central-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "pew-pew-pew-remote"
+    key = "pewpewpew"
+    region = "eu-central-1"
+  }
+}
+
+resource "aws_s3_bucket" "s3Bucket" {
+  bucket = "pew-pew-pew"
+  acl = "public-read"
+
+  policy = <<EOF
+{
+  "Id": "MakePublic",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::pew-pew-pew/*",
+      "Principal": "*"
+    }
+  ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+  }
+}
